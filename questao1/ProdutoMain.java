75af0d936dced0e6f8b15f4e54ada79e556be9d7
GustavoCBittencourt <165939186+GustavoCBittencourt@users.noreply.github.com>
2024-04-17 16:55:28 -0300

    Add files via upload

---
+++ questao1/ProdutoMain.java
@@ -0,0 +1,10 @@
+package questao1;
+
+public class ProdutoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ questao1/smartphone.java
@@ -0,0 +1,18 @@
+package questao1;
+
+public class smartphone extends produto{
+
+	private String tela;
+
+	public String getTela() {
+		return tela;
+	}
+
+	public void setTela(String tela) {
+		this.tela = tela;
+	}
+		
+
+	}
+
+
